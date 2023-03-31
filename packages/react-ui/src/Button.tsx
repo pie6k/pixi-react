3d06c9670ea8be7118f2e28714598101651566dc
Alex Prokop <alex.prokop@play.co>
2023-03-31 15:42:01 +0100

    update Button based on new pixi-ui API

--- packages/react-ui/src/Button.tsx
+++ packages/react-ui/src/Button.tsx
@@ -1,9 +1,16 @@
-import { cloneElement, useEffect, useRef } from 'react';
+import { cloneElement, useEffect, useRef, useMemo, useCallback } from 'react';
 import { Button as PixiButton } from '@pixi/ui';
 import type { FederatedPointerEvent } from '@pixi/events';
 import type { Container } from '@pixi/display';
 
-type ButtonSignalName = 'onPress' | 'onPress' | 'onDown' | 'onUp' | 'onHover' | 'onOut' | 'onUpOut';
+type ButtonSignalName =
+    | 'onPress'
+    | 'onPress'
+    | 'onDown'
+    | 'onUp'
+    | 'onHover'
+    | 'onOut'
+    | 'onUpOut';
 type SignalHandler = (btn?: PixiButton, e?: FederatedPointerEvent) => void;
 
 export type ButtonProps = {
@@ -20,48 +27,48 @@ export type ButtonProps = {
 const noop = () => {};
 
 const useSignalEffect = (
-    buttonInstance: React.RefObject<PixiButton>,
+    buttonInstance: PixiButton,
     signalName: ButtonSignalName,
-    handler: SignalHandler,
+    handler: SignalHandler
 ) =>
     useEffect(() =>
     {
-        if (!buttonInstance.current)
+        if (!buttonInstance)
         {
             return;
         }
 
-        buttonInstance.current[signalName].connect(handler);
+        buttonInstance[signalName].connect(handler);
 
         // eslint-disable-next-line consistent-return
         return () =>
         {
-            if (!buttonInstance.current)
+            if (!buttonInstance)
             {
                 return;
             }
 
-            buttonInstance.current[signalName].disconnect(handler);
+            buttonInstance[signalName].disconnect(handler);
         };
     }, [signalName, handler]);
 
 /**
- * This is a slightly funky implementation. The pixi-ui Button Class is just a plain JS Class and NOT an extension of
- * Container. It also requires a view that extends Container to be passed through to its constructor. Because of this:
+ * The pixi-ui Button Class is just a plain JS Class and NOT an extension of Container. It also requires a view that extends
+ * Container to be passed to it. Because of this:
  *
- * 1) We need to somehow instantiate Pixi UI Button outside the PIXI scenegraph
- * 2) We need to have an instantiated view component (not JSX) ready to pass to it when we instantiate it
- * 3) We can't use a custom pixi-react PixiComponent to do this as it won't allow us to do 1) or 2). 1) Because PixiComponent
- *    needs to return a PixiReactContainer not just a plain Object; 2) Because any pixi-react component passed in via a prop
- *    (view or children) will be JSX and NOT a PixiReactContainer instance because the pixi-react Reconciler's hostConfig
- *    createInstance method isn't called until the JSX is actually rendered.
- * 4) react-three-fiber solves part of the problem of non-scenegraph objects using its attach prop, but this relies on
- *    attaching non-scenegraph objects to scenegraph objects, we want to do the opposite
- * 5) We could potentially allow PixiComponent to return plain Objects and pass through a PixiReactContainer defined on a
- *    child JSX element via an attach prop as its scenegraph component, but it's a big refactor
+ * - We need to instantiate Pixi UI Button outside the PIXI scenegraph
+ * - We need to have an instantiated view component (not JSX) to pass to it
+ * - We can't use a custom pixi-react PixiComponent to do this as it won't allow us to do 1) or 2). 1) Because PixiComponent
+ *   needs to return a PixiReactContainer not just a plain Object; 2) Because any pixi-react component passed in via a prop
+ *   (view or children) will be JSX and NOT a PixiReactContainer instance because the pixi-react Reconciler's hostConfig
+ *   createInstance method isn't called until the JSX is actually rendered.
  *
- * To solve this, we're creating a React Component wrapper, since it's easy to add non-scenegraph components in React which
- * return child PixiReactContainer instances:
+ * To solve this we use a standard React Functional Component as a wrapper, since this is already outside the pixi scenegraph:
+ *
+ * 1) Instantiate Pixi UI Button in a `useMemo`
+ * 2) Return `props.children` to offload rendering to PixiReactFiber
+ * 3) Retrieve the resultant Pixi Component Instance via a callback ref and pass it to `PixiButton.init`
+ * 4) Update props via `useEffect`
  */
 export const Button = (props: ButtonProps) =>
 {
@@ -75,20 +82,27 @@ export const Button = (props: ButtonProps) =>
         onOut = noop,
         onUpOut = noop,
     } = props;
-    const viewInstance = useRef<Container>(null!);
-    const buttonInstance = useRef<PixiButton>(null!);
+    // 1) instantiate an empty PixiButton
+    const buttonInstance = useMemo<PixiButton>(() => new PixiButton(), []);
 
-    // 2) instantiate a pixi-ui Button with the child view on initial mount
+    // 3) initialize the buttonInstance with the rendered child
+    const handleViewUpdate = useCallback(
+        (viewInstance: Container) =>
+        {
+            if (!viewInstance)
+            {
+                return;
+            }
+            buttonInstance.init(viewInstance);
+        },
+        [buttonInstance]
+    );
+
+    // 4) update any props whenever they change
     useEffect(() =>
     {
-        buttonInstance.current = new PixiButton(viewInstance.current);
-    }, []);
-
-    // 3) update any props whenever they change
-    useEffect(() =>
-    {
-        buttonInstance.current.enabled = enabled;
-    }, [enabled]);
+        buttonInstance.enabled = enabled;
+    }, [buttonInstance, enabled]);
 
     useSignalEffect(buttonInstance, 'onPress', onPress);
     useSignalEffect(buttonInstance, 'onDown', onDown);
@@ -97,8 +111,8 @@ export const Button = (props: ButtonProps) =>
     useSignalEffect(buttonInstance, 'onOut', onOut);
     useSignalEffect(buttonInstance, 'onUpOut', onUpOut);
 
-    // 1) Render the passed in JSX pixi-react component from props.children, capturing it via a ref
+    // 2) Render the passed in JSX pixi-react component from props.children, using a callback ref
     return cloneElement(children, {
-        ref: viewInstance,
+        ref: handleViewUpdate,
     });
 };
